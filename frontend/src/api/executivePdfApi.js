7237150e1a2c0887c8d0baca2535b1ed2a9cbba9
Bayarbat Batdorj <bayarbat.batdorj@gmail.com>
2026-08-07 14:01:19 +0800

    Fix executive PDF API import for production build

--- frontend/src/api/executivePdfApi.js
+++ frontend/src/api/executivePdfApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const { API_BASE_URL } from "../config/apiConfig";
+import { API_BASE_URL } from "../config/apiConfig";
 
 export async function exportExecutiveKpiPdf({
   kpiKey,
